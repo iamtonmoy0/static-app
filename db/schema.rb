51bc5c41a0d8cd26b72f1e9082e9c50109a3d533
iamtonmoy0 <tonmoy2code@gmail.com>
2023-02-13 14:20:26 -0500

    db migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_30_200538) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_13_191812) do
   create_table "friends", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -18,6 +18,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_30_200538) do
     t.string "twitter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_friends_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
